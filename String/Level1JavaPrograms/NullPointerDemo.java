5331c98e78491fd052ed832830dae38831347e39
unknown <rhythem>
2025-09-03 13:39:43 +0530

    stringassignment

---
+++ String/Level1JavaPrograms/NullPointerDemo.java
@@ -0,0 +1,18 @@
+public class NullPointerDemo {
+    public static void generateException() {
+        String text = null;
+        System.out.println(text.length());
+    }
+    public static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Handled NullPointerException");
+        }
+    }
+    public static void main(String[] args) {
+        // generateException();
+        handleException();
+    }
+}
